40b6862be889fa3cb5c273fb93acdc553b3ac76a
Joctox04 <99295799+Joctox04@users.noreply.github.com>
2023-06-02 02:16:20 -0600

    algunos avances

--- recreador-imagen.js
+++ recreador-imagen.js
@@ -1,5 +1,14 @@
 const Jimp = require('jimp');
 
+const generaciones = 10;
+
+//48672 es el numero de similitudes que estamos teniendo
+const imagePath1 = 'public/imagen_objetivo.png';  //imagen del usuario
+const arr2 = [{ x: 2, y: 3 }, { x: 5, y: 6 }, { x: 8, y: 9 }];
+
+const imagePath = 'public/imagen_final.png';     //base de la imagen, imagen blanca para escribir
+const coordenadas = [{ x: 2, y: 3 }, { x: 5, y: 6 }, { x: 8, y: 9 }];
+
 async function compareImages(imagePath1, imagePath2) {
   const image1 = await Jimp.read(imagePath1);
   const image2 = await Jimp.read(imagePath2);
@@ -31,8 +40,6 @@ async function compareImages(imagePath1, imagePath2) {
   return score; 
 }
 
-const coordenadasPuntosNegros = [];
-
 async function getCoordenadasPuntosNegros(imagePath1) {
   const image1 = await Jimp.read(imagePath1);
 
@@ -46,21 +53,16 @@ async function getCoordenadasPuntosNegros(imagePath1) {
 
 
       if (pixel1.r === 6 && pixel1.g === 6 && pixel1.b === 8) {
-        coordenadasPuntosNegros.push({ x, y });
+        XandY_PuntosNegros.push({ x, y });
       }
     }
   }
 
-  return coordenadasPuntosNegros;
+  return XandY_PuntosNegros;
 }
 
-
-
-async function findCommonElements(imagePath1, arr2) {
-  const image1 = await Jimp.read(imagePath1);
+function findCommonElements(image1, comparador,width,height) {
   const puntosNegros = [];
-  const width = image1.getWidth();
-  const height = image1.getHeight();
 
   for (let y = 0; y < height; y++) {
     for (let x = 0; x < width; x++) {
@@ -73,59 +75,175 @@ async function findCommonElements(imagePath1, arr2) {
   }
 
   const commonElements = puntosNegros.filter(element => {
-    return arr2.some(item => item.x === element.x && item.y === element.y);
+    return comparador.some(item => item.x === element.x && item.y === element.y);
   });
   console.log(commonElements.length)
   return commonElements.length;
 }
 
-const imagePath1 = 'public/imagen_objetivo.png';
-const arr2 = [{ x: 2, y: 3 }, { x: 5, y: 6 }, { x: 8, y: 9 }];
-
-findCommonElements(imagePath1, arr2)
-  .then(similarityCount => {
-    console.log('Cantidad de similitudes encontradas:', similarityCount);
-  })
-  .catch(error => {
-    console.error('', error);
-  });
-
-//const imagePath2 = 'public/rojo.jpg';
-//compareImages(imagePath1, imagePath2);
-
-
-
-
 async function crearImagen(imagePath, coordenadas) {
   const image = await Jimp.read(imagePath);
   const black = Jimp.cssColorToHex('#000000');
+
+  //console.log(coordenadas)
 
   coordenadas.forEach(coord => {
     const { x, y } = coord;
     image.setPixelColor(black, x, y);
   });
 
-  const outputImagePath = 'output.png';
+  const outputImagePath = 'public/imagenFinal.png';
   await image.writeAsync(outputImagePath);
 
   console.log(`Se agregaron píxeles negros a la imagen`);
 }
 
-const imagePath = 'public/imagen_final.png';
-const coordenadas = [{ x: 2, y: 3 }, { x: 5, y: 6 }, { x: 8, y: 9 }];
-
-crearImagen(imagePath, coordenadas)
-  .catch(error => {
-    console.error('Ocurrió un error:', error);
-  });
-
-
+//findCommonElements(imagePath1, arr2)
+//  .then(similarityCount => {
+//    console.log('Cantidad de similitudes encontradas:', similarityCount);
+//  })
+//  .catch(error => {
+//    console.error('', error);
+//});
 
 
-getCoordenadasPuntosNegros(imagePath1);
+function compararArrays(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
 
-//const commonCount = findCommonElements([1,2,3], [1,2,3]);
-//console.log(`Cantidad de elementos en común: ${commonCount}`);
+  //for (let i = 0; i < array1.length; i++) {
+ //   if (array1[i].x !== array2[i].x || array1[i].y !== array2[i].y) {
+   //   return false;
+  //  }
+ // }
+
+  return true;
+}
+
+function verificarCoordenadas(coordenadas, x, y) {
+  return coordenadas.some(coordenada => coordenada.x === x && coordenada.y === y);
+}
+
+function generarNumeroAleatorio(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 
+function agregaXandY(arrayObjetivo, arrayFinal, padre){
 
+  // significa que x y y van en esa posicion para la imagen
+  if (verificarCoordenadas(arrayObjetivo, padre[0].x, padre[0].y)) {
+    //console.log(`Las coordenadas (${padre[0].x}, ${padre[0].y}) están en el array.`);
+    arrayFinal.push(padre[0]);  
+  }
+  return arrayFinal
+
+}
+
+
+/**
+ * Funcion para crear un array del tamaño del array de la imagen objetivo
+ * @param {*} array 
+ * @param {*} width 
+ * @param {*} height 
+ * @returns 
+ */
+function generarArray(array, width, height){
+
+  for (let y = 0; y < height; y++) {
+    for (let x = 0; x < width; x++) {
+
+      //x = generarNumeroAleatorio(1, width),
+      //y = generarNumeroAleatorio(1, height)
+      
+      
+      array.push({ x, y });
+
+    }
+  }
+  
+  return array
+}
+
+function mutacion(arrayObjetivo, arrayComparador, width, height){
+  for (let y = 0; y < height; y++) {
+    for (let x = 0; x < width; x++) {
+      // Los valores son iguales, entonces no los modificamos
+      if ( arrayObjetivo[x].x === arrayComparador[x].x 
+        && arrayObjetivo[y].y === arrayComparador[y].y) {
+        break; 
+      }
+      x = generarNumeroAleatorio(1, width)
+      y = generarNumeroAleatorio(1, height)
+
+      arrayComparador[x].x = 500
+      arrayComparador[y].y = 500
+
+    }
+  }
+
+  return arrayComparador
+}
+
+async function runGeneticAlgorithm() {
+  //-------------------------------------------------------------------------------------
+  //CREAMOS LA VARIABLE PARA ALMACENAR LOS PUNTOS X y Y de la imagen del usuario
+  const puntosNegros = []                      // array de la imagen del usuario
+  const image1 = await Jimp.read(imagePath1);  // imagen del usuario
+  const width = image1.getWidth();             // ancho
+  const height = image1.getHeight();           // alto
+
+  for (let y = 0; y < height; y++) {
+    for (let x = 0; x < width; x++) {
+      const pixel1 = Jimp.intToRGBA(image1.getPixelColor(x, y));
+      if (pixel1.r === 6 && pixel1.g === 6 && pixel1.b === 8) {
+        puntosNegros.push({ x, y });
+      }
+    }
+  }
+
+
+  //-------------------------------------------------------------------------------------
+  
+  let i = 1
+  let puntosNegrosFinal = [] //array para la creacion de la imagen
+  let padre = []
+  // array para utilizarlo para crear la imagen
+  //padre = [{x: 3, y: 3}]
+  //madre = [{x: 4, y: 6}]
+
+  // realizamos el ciclo para intentar recrear la imagen de manera genetica
+  while (i <= 5000){
+
+    //if(compararArrays(puntosNegros, puntosNegrosFinal)){   // si es true, sale, ya que son iguales
+    //  console.log("Fueron iguales")  
+    //  return
+    //}
+
+    // Se crean x y y aleatorios
+    padre = [{x: generarNumeroAleatorio(1, width), y: generarNumeroAleatorio(1, height)}]
+    madre = [{x: generarNumeroAleatorio(1, width), y: generarNumeroAleatorio(1, height)}]
+
+    //console.log(padre[0])
+
+    puntosNegrosFinal = agregaXandY(puntosNegros, puntosNegrosFinal, padre)
+    //puntosNegrosFinal.push(padre[0]);
+    //console.log(puntosNegrosFinal[0])
+    //puntosNegrosFinal = mutacion(puntosNegros, puntosNegrosFinal, width, height)
+
+    findCommonElements(image1, puntosNegrosFinal, width, height) // imprime las similitudes que tenga la imagen del usuario y la imagen final
+    i++
+  }
+
+  //console.log(puntosNegrosFinal)
+
+  //crearImagen(imagePath, puntosNegrosFinal)
+  //  .catch(error => {
+  //    console.error('Ocurrió un error:', error);
+  //});
+
+  return
+}
+
+runGeneticAlgorithm();
